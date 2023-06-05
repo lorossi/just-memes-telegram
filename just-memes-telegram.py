24fc1dcf4eefb565baf408dab1c51779c8368c58
lorossi <mail@lorenzoros.si>
2023-06-05 17:18:00 +0200

    replaced the main script

--- just-memes-telegram.py
+++ just-memes-telegram.py
@@ -1,29 +1,48 @@
+import asyncio
 import logging
 import tracemalloc
 
 from modules.telegrambot import TelegramBot
 
 
-def main():
+async def idle():
+    """Do nothing. This is a coroutine that is used to keep the event loop running."""
+    while True:
+        await asyncio.sleep(1)
+
+
+async def main():
     """Start the bot. Function automatically called whenever the script is run."""
     format = (
         "%(asctime)s - %(levelname)s - %(module)s (%(lineno)d, in %(funcName)s) "
         "- %(message)s"
     )
-    filename = __file__.replace(".py", ".log")
     logging.basicConfig(
         level=logging.INFO,
         format=format,
-        filename=filename,
         filemode="w",
+        filename=__file__.replace(".py", ".log"),
     )
     tracemalloc.start()
 
-    logging.info("Script started.")
+    logging.info("Script started.c")
     t = TelegramBot()
-    logging.info("Telegram initialized.")
-    t.start()
+
+    logging.info("Starting bot...")
+    await t.startAsync()
+    logging.info("Bot started.")
+
+    try:
+        await idle()
+    except asyncio.exceptions.CancelledError:
+        logging.warning("SIGINT received. Stopping bot...")
+
+    logging.info("Stopping bot...")
+    await t.stopAsync()
+    logging.info("Bot stopped.")
+
+    logging.info("Script ended.")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
